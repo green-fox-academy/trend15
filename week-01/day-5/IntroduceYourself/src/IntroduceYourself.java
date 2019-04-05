1e7b9b36797177cbc3426ac64a0fa6166dbf1d24
Milan <trend15@gmail.com>
2019-04-05 11:57:19 +0200

    The Program shows the Name

---
+++ week-01/day-5/IntroduceYourself/src/IntroduceYourself.java
@@ -0,0 +1,11 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        int age = 35;
+        String name = "Mimon Milan";
+        double height = 1.82;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
