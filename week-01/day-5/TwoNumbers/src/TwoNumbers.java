3a8adf1d1d6849a3ece218434ca75bfc3ffdcc48
Milan <trend15@gmail.com>
2019-04-05 12:57:34 +0200

    The Program makes mathematics between two number

---
+++ week-01/day-5/TwoNumbers/src/TwoNumbers.java
@@ -0,0 +1,14 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a,b;
+        a=13;
+        b=22;
+        float j = b / a;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(b*a);
+        System.out.println(j);
+        System.out.println(b / a);
+        System.out.println(b % a);
+    }
+}
