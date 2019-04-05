e585f96896ac1fb8d38f2c866dfdd3ff1b24372c
Milan <trend15@gmail.com>
2019-04-05 11:46:46 +0200

    Defines the Basic Information about myself

---
+++ week-01/day-5/DefineBasicInfo/src/DefineBasicInfo.java
@@ -0,0 +1,11 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Mimon Milan";
+        int age = 35;
+        double heigth = 182.02;
+        boolean married = false;
+
+        System.out.println( "My name is: " + name + " and my age is: " + age + " my heigth is: " + heigth);
+        System.out.println("Iam married ? " + married);
+    }
+}
