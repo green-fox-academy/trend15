d5bb0332b49bae76372ff2d807a98cefb015c80c
Milan <trend15@gmail.com>
2019-04-06 19:02:31 +0200

    The program uses a method to greet

---
+++ week-01/day-5/Greet/src/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+    public static void main(String[] args) {
+        String a1 = "Greenfox";
+        greet(a1);
+    }
+
+    private static void greet(String a1) {
+        System.out.println("Greetings dear, "+a1);
+    }
+}
