7c43772f9920c97df652c369311a7b4cede5f61d
Milan <trend15@gmail.com>
2019-04-08 08:36:58 +0200

    The Program uses a 2D array to store the shades

---
+++ week-02/day-1/Colors/src/Colors.java
@@ -0,0 +1,8 @@
+public class Colors {
+    public static void main(String[] args) {
+        String[][] shades =   {{"lime", "forest green", "olive", "pale green", "spring green"},
+                                {"orange red", "red", "tomato"},
+                                {"orchid", "violet", "pink", "hot pink"}};
+        System.out.println(shades[2][0]);
+    }
+}
