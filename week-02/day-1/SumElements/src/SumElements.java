1aa35646246198ad757c7acb7e905cfe68b1cd39
Milan <trend15@gmail.com>
2019-04-08 08:00:04 +0200

    The Program makes a sum with the elemenets of array

---
+++ week-02/day-1/SumElements/src/SumElements.java
@@ -0,0 +1,12 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int[] r = {54, 23, 66, 12};
+        int sum =0;
+        for (int i= 2;i<=3;i++){
+            sum+=r[i];
+        }
+        System.out.println("The sum is: "+ sum);
+    }
+}
+
+// - Print the sum of the second and the third element
