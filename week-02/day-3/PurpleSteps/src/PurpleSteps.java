8ed4fe0d39679f68d3b48e3f91c5fa098ff01a5f
Milan <trend15@gmail.com>
2019-04-10 16:12:19 +0200

    The Program draws a kind of diagonal which stays in 19 squares and has a purple color

---
+++ week-02/day-3/PurpleSteps/src/PurpleSteps.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+
+        int x=10;
+        for(int i=0;i<19;i++) {
+            graphics.setColor(new Color(218,112,214));
+            graphics.fillRect(x, x, 10, 10);
+            x += 10;
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
