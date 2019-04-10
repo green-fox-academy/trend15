204c91870b6c0545e344089e5d10e34711b31edf
Milan <trend15@gmail.com>
2019-04-10 16:36:29 +0200

    The Program draws a kind of diagonal which stays in 6 squares and has a purple color - square sizes are incraesing

---
+++ week-02/day-3/PurpleSteps3d/src/PurpleSteps3d.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3d {
+    public static void mainDraw(Graphics graphics) {
+        int x=4;
+        for (int i =0;i<6;i++) {
+            graphics.setColor(new Color(255, 0, 255));
+            graphics.fillRect(x, x,x,x);
+            x*=2;
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
