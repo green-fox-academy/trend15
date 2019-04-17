192e5f295661cda4252baa4efb6044564a5e9fe5
Milan <trend15@gmail.com>
2019-04-17 11:54:58 +0200

    The Program uses classes to make animal objects which can be increase or decrease the thirst or hunger

---
+++ week-03/day-3/Animal/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.Print();
+        dog.eat();
+        dog.drink();
+        dog.Print();
+        dog.play();
+        dog.play();
+        dog.play();
+        dog.play();
+        System.out.println(dog.hunger);
+        System.out.println(dog.thirst);
+    }
+}
