77c29b49051ddf9ef8b14781408d4e5be409ab66
Milan <trend15@gmail.com>
2019-04-17 15:57:44 +0200

    The Program uses the this key to makes several math classes

---
+++ week-03/day-3/Counter/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Counter basic = new Counter();
+        basic.add(50);
+        basic.add();
+        basic.get();
+    }
+}
