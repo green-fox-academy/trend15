31bade9bedab954c34ba295489032277f43e4759
Milan <trend15@gmail.com>
2019-05-14 11:35:10 +0200

    with stream we find the even numbers n ->n%2

---
+++ week-05/day-01/streamEvenNumbers/src/main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class main {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+  }
+}
